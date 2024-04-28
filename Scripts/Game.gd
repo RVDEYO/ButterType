9d71984dac3f7cf95138b7d95dec4c6e89faf817
RVDEYO <68681800+RVDEYO@users.noreply.github.com>
2024-04-28 13:07:01 -0400

    Fully added code comments, fixed _nextWord scenario

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -26,7 +26,7 @@ func gameSetup():
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed():
 		var key_typed = PackedByteArray([event.unicode]).get_string_from_ascii()
-		iString = iString + key_typed
+		
 		
 		if event.keycode == KEY_BACKSPACE: # If Backspace was pressed
 			_checkBackspace()
@@ -35,6 +35,9 @@ func _input(event: InputEvent) -> void:
 		if event.keycode == KEY_SPACE: # If Space was pressed
 			_nextWord()
 			return
+		
+		# Adds the user inputted character to the iString
+		iString = iString + key_typed
 		
 		# Keys that are not characters have a unicode of 0
 		# Stops invisible characters from being inputted
@@ -93,45 +96,60 @@ func _checkBackspace():
 
 # Deletes the current letter that the user has inputted
 func _deleteLetter():
+	# Sets the boolean variable indicator for the inputted letter to null
 	gameArray[currentWord][iString.length()-1][2] = null
+	# Sets the variable that holds what the user inputted to null
 	gameArray[currentWord][iString.length()-1][1] = null
 	iString = iString.left(-1)
 	char_deleted.emit(false)
 
 # Handles moving to the previous word
 func _prevWord():
+	# Verifies that the currentWord isn't the first word
 	if currentWord != 0:
+		# Checks if the previous word is not true, if it is we can't go back to the word
 		if gameArray[currentWord-1][gameArray[currentWord-1].size()-1] != true:
 			# The word is back in action, it is now neither true or false
 			gameArray[currentWord-1][gameArray[currentWord-1].size()-1] = null
 			currentWord -= 1
+			# Iterates through the currentWord
 			for i in range(0, gameArray[currentWord].size()-1):
+				# Break out of the loop once loop reaches a null letter (nothing inputted in its place)
 				if gameArray[currentWord][i][2] == null:
 					break
+				# Takes the letters already inputted and put them back in iString
 				iString += gameArray[currentWord][i][2]
 			prev_word.emit()
 
 # Handles moving to the next word
 func _nextWord():
 	# Makes sure you can't just skip a word without having any input in it
-	## Doesn't work
 	if iString.length() != 0:
 		# Checks if the current word is the last word
 		if currentWord == gameArray.size()-1:
 			gameOver()
 		else:
+			# Iterates through the letters of the currentWord
 			for i in range(0, gameArray[currentWord].size()-1):
+				# Once it finds a false or null letter, set the currentWord to false and break loop
 				if gameArray[currentWord][i][1] == false or gameArray[currentWord][i][1] == null:
 					gameArray[currentWord][gameArray[currentWord].size()-1] = false
 					break
 				else:
+					# If no letters in currentWord are false or null, set currentWord to true
 					gameArray[currentWord][gameArray[currentWord].size()-1] = true
+			
+			# Checking results of the loop above, if the word is true, emit correct word signal
 			if gameArray[currentWord][gameArray[currentWord].size()-1] == true:
 				correct_word_submitted.emit()
+			# If the word is false, emit the incorrect word signal
 			else:
 				incorrect_word_submitted.emit()
+
+			# Move to the next Word
 			currentWord += 1
 			iString = ""
+
 
 # Handles when the game is over, will run final calculations and send them out
 func gameOver():
